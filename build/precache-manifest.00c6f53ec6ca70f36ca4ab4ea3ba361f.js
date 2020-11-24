28bb4dcc2fa54d7d7d7d756def4cc3ee6e56178f
aleebrady <aleebrady@gmail.com>
2020-11-24 12:13:23 -0800

    Done.

---
+++ build/precache-manifest.00c6f53ec6ca70f36ca4ab4ea3ba361f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af0a5d026a4d80a41a49",
+    "url": "/static/css/main.fbb1e1c1.chunk.css"
+  },
+  {
+    "revision": "af0a5d026a4d80a41a49",
+    "url": "/static/js/main.146e9411.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "76a187f90543d5d345c9743d30441e32",
+    "url": "/index.html"
+  }
+];
